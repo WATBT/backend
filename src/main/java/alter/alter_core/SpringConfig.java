a6f748060a8b09444958b3e19d0a6d1a9890af81
Suah-Cho <sacho0929@gmail.com>
2024-12-22 19:29:40 +0900

    feat: #1 기본 기능 개발을 위한 jpa 설정 진행

---
+++ src/main/java/alter/alter_core/SpringConfig.java
@@ -0,0 +1,30 @@
+package alter.alter_core;
+
+import alter.alter_core.repository.JpaMemberRepository;
+import alter.alter_core.repository.MemberRepository;
+import alter.alter_core.service.MemberService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JpaMemberRepository(em);
+    }
+}
